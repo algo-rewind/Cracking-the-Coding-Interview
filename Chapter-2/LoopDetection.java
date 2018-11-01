813a62cf96679e500e269db9ba9228b42290f770
Dyavanapalli <AnandShankar.Dyavanapalli@allscripts.com>
2018-11-01 10:17:18 -0400

    Clean-up repository for a re-write in C#.

--- Chapter-2/LoopDetection.java
+++
@@ -1,9 +0,0 @@
-/**
- * 2.8: Loop Detection
- * Given a circular linked list, implement an algorithm that returns the node at
- * the beginning of the loop.
- */
-public class LoopDetection {
-    public static void main(String[] args) {
-    }
-}
